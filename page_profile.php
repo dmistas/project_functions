9800fd90c273f7dbfdf89f61f9bfd165f645da7d
Stanislav Dmitriev <dmistas@mail.ru>
2021-02-01 10:37:25 +0500

    add user_profile

--- page_profile.php
+++ page_profile.php
@@ -1,11 +1,23 @@
 <?php
 session_start();
 include_once 'functions.php';
-if (isset($_GET['id'])){
-    $id = intval($_GET['id']);
-    echo "Здесь будет профиль пользователя"."<br>";
-    d(get_user_by_id($id), 0);
+if (is_not_logged_in()) {
+    redirect_to('page_login.php');
+    exit();
 }
+// если есть GET['id'] переходим на профиль по GET['id'] иначе по id авторизованного пользователя
+$id = isset($_GET['id']) ? intval($_GET['id']) : $_SESSION['user']['id'];
+$user = get_user_by_id($id);
+$name = $user['name']?$user['name']:"";
+$email = $user['email']?$user['email']:"";
+$phone = $user['phone']?$user['phone']:"";
+$job_title = $user['job_title']?$user['job_title']:"";
+$address = $user['address']?$user['address']:"";
+$status = $user['status']?$user['status']:"";
+$vk = $user['vk']?$user['vk']:"";
+$telegram = $user['telegram']?$user['telegram']:"";
+$instagram = $user['instagram']?$user['instagram']:"";
+$img = $user['img']?$user['img']:"";
 ?>
 
 <!DOCTYPE html>
@@ -23,82 +35,73 @@ if (isset($_GET['id'])){
     <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
     <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
 </head>
-    <body class="mod-bg-1 mod-nav-link">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-            <a class="navbar-brand d-flex align-items-center fw-500" href="#"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarColor02">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item ">
-                        <a class="nav-link" href="#">Главная</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Войти</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Выйти</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <main id="js-page-content" role="main" class="page-content mt-3">
-            <div class="subheader">
-                <h1 class="subheader-title">
-                    <i class='subheader-icon fal fa-user'></i> Иван Иванов
-                </h1>
-            </div>
-            <div class="row">
-              <div class="col-lg-6 col-xl-6 m-auto">
-                    <!-- profile summary -->
-                    <div class="card mb-g rounded-top">
-                        <div class="row no-gutters row-grid">
-                            <div class="col-12">
-                                <div class="d-flex flex-column align-items-center justify-content-center p-4">
-                                    <img src="img/demo/avatars/avatar-admin-lg.png" class="rounded-circle shadow-2 img-thumbnail" alt="">
-                                    <h5 class="mb-0 fw-700 text-center mt-3">
-                                        Иван Иванов 
-                                        <small class="text-muted mb-0">Toronto, Canada</small>
-                                    </h5>
-                                    <div class="mt-4 text-center demo">
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#C13584">
-                                            <i class="fab fa-instagram"></i>
-                                        </a>
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#4680C2">
-                                            <i class="fab fa-vk"></i>
-                                        </a>
-                                        <a href="javascript:void(0);" class="fs-xl" style="color:#0088cc">
-                                            <i class="fab fa-telegram"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="p-3 text-center">
-                                    <a href="tel:+13174562564" class="mt-1 d-block fs-sm fw-400 text-dark">
-                                        <i class="fas fa-mobile-alt text-muted mr-2"></i> +1 317-456-2564</a>
-                                    <a href="mailto:oliver.kopyov@marlin.ru" class="mt-1 d-block fs-sm fw-400 text-dark">
-                                        <i class="fas fa-mouse-pointer text-muted mr-2"></i> oliver.kopyov@marlin.ru</a>
-                                    <address class="fs-sm fw-400 mt-4 text-muted">
-                                        <i class="fas fa-map-pin mr-2"></i> Восточные Королевства, Штормград 15
-                                    </address>
-                                </div>
+<body class="mod-bg-1 mod-nav-link">
+<?php include 'nav_component.php' ?>
+<main id="js-page-content" role="main" class="page-content mt-3">
+    <?php
+    if (isset($_SESSION['success'])) {
+        display_flash_message("success");
+    }
+    if (isset($_SESSION['danger'])) {
+        display_flash_message("danger");
+    }
+    ?>
+    <div class="subheader">
+        <h1 class="subheader-title">
+            <i class='subheader-icon fal fa-user'></i> <?= $name ?>
+        </h1>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-xl-6 m-auto">
+            <!-- profile summary -->
+            <div class="card mb-g rounded-top">
+                <div class="row no-gutters row-grid">
+                    <div class="col-12">
+                        <div class="d-flex flex-column align-items-center justify-content-center p-4">
+                            <img src="<?= $img ?>"
+                                 class="rounded-circle shadow-2 img-thumbnail" alt="">
+                            <h5 class="mb-0 fw-700 text-center mt-3">
+                                <?= $name ?>
+                                <small class="text-muted mb-0"><?= $address ?></small>
+                            </h5>
+                            <div class="mt-4 text-center demo">
+                                <a href="javascript:void(0);" class="fs-xl" style="color:#C13584">
+                                    <i class="fab fa-instagram"><?= $instagram ?></i>
+                                </a>
+                                <a href="javascript:void(0);" class="fs-xl" style="color:#4680C2">
+                                    <i class="fab fa-vk"><?= $vk ?></i>
+                                </a>
+                                <a href="javascript:void(0);" class="fs-xl" style="color:#0088cc">
+                                    <i class="fab fa-telegram"><?= $telegram ?></i>
+                                </a>
                             </div>
                         </div>
                     </div>
-               </div>
+                    <div class="col-12">
+                        <div class="p-3 text-center">
+                            <a href="tel:<?= $phone ?>" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                <i class="fas fa-mobile-alt text-muted mr-2"></i> <?= $phone ?></a>
+                            <a href="mailto:oliver.kopyov@marlin.ru" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                <i class="fas fa-mouse-pointer text-muted mr-2"></i> <?= $email ?></a>
+                            <address class="fs-sm fw-400 mt-4 text-muted">
+                                <i class="fas fa-map-pin mr-2"></i> <?= $address ?>
+                            </address>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </main>
-    </body>
+        </div>
+    </div>
+</main>
+</body>
 
-    <script src="js/vendors.bundle.js"></script>
-    <script src="js/app.bundle.js"></script>
-    <script>
+<script src="js/vendors.bundle.js"></script>
+<script src="js/app.bundle.js"></script>
+<script>
 
-        $(document).ready(function()
-        {
+    $(document).ready(function () {
 
-        });
+    });
 
-    </script>
+</script>
 </html>
